be0cc70caa8f3258a2ad5bf1ef6dfe64b075931d
wrathematics <wrathematics@gmail.com>
2013-08-02 09:14:10 -0400

    08.02.13 split again for talk

--- pbdr/common/scripts/pbdDMAT/dmat_examples/2_gen.r
+++ pbdr/common/scripts/pbdDMAT/dmat_examples/2_gen.r
@@ -1,11 +1,13 @@
 library(pbdDMAT, quiet=TRUE)
 init.grid()
 
-zero.dmat <- ddmatrix(0, nrow=100, ncol=100)
+zero.dmat <- ddmatrix(0, nrow=10, ncol=10)
 zero.dmat
 
-id.dmat <- diag(1, nrow=100, ncol=100, type="ddmatrix")
+id.dmat <- diag(1, nrow=10, ncol=10, type="ddmatrix")
 id.dmat
+
+comm.print(submatrix(id.dmat), all.rank=T)
 
 comm.set.seed(diff=T)
 rand.dmat <- ddmatrix("rnorm", nrow=100, ncol=100, mean=10, sd=100)
