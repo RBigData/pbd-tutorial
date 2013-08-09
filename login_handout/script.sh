61489258f9e1968dc4006e33b62ed7f4903492e0
wrathematics <wrathematics@gmail.com>
2013-08-09 16:52:11 -0400

    08.09.13 updated script

--- login_handout/script.sh
+++ login_handout/script.sh
@@ -5,22 +5,23 @@ sc=`uname -a | awk '{ print $2 }'`
 outdir="${HOME}/pbdR_examples/"
 
 if [ ! -d "${outdir}" ]; then
-	mkdir $outdir
+  mkdir $outdir
 fi
 
 
 # Nautilus
 if [ $sc = "conseil" -o $sc = "nautilus" ]; then
-	path="/path/to/scripts.tar.gz"
-	tmpdir="/lustre/wherever"
+  path="/path/to/scripts.tar.gz"
+  tmpdir="/lustre/wherever"
 # Kraken
-#elif [ $sc = "
+elif [ `$sc | sed 's/.$//' = "krakenpf" ]; then
+  path="/path/to/scripts.tar.gz"
+  tmpdir="lustre/wherever"
 fi
 
 
-
 if [ ! -d $tmpdir ]; then
-	mkdir ${tmpdir}/R_TEMP/
+  mkdir ${tmpdir}/R_TEMP/
 fi
 
 
@@ -28,4 +29,5 @@ cp ${path} ${outdir}
 cd ${outdir}
 tar xvf scripts.tar.gz
 cd scripts/
+
 
