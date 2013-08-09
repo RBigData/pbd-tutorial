6361c324554c128593d4918a6d2e2a81232e90a7
wrathematics <wrathematics@gmail.com>
2013-08-09 17:05:42 -0400

    08.09.13 added chester and lens

--- login_handout/script.sh
+++ login_handout/script.sh
@@ -8,15 +8,25 @@ if [ ! -d "${outdir}" ]; then
   mkdir $outdir
 fi
 
+# remove last character (login node number)
+scl1=`echo $sc | sed 's/.$//'`
 
 # Nautilus
 if [ $sc = "conseil" -o $sc = "nautilus" ]; then
   path="/path/to/scripts.tar.gz"
   tmpdir="/lustre/wherever"
 # Kraken
-elif [ `echo $sc | sed 's/.$//'` = "krakenpf" ]; then
+elif [ $scl1 = "krakenpf" ]; then
   path="/path/to/scripts.tar.gz"
   tmpdir="lustre/wherever"
+# Lens
+elif [ $scl1 = "lens-login" ]; then
+  path=""
+  tmpdir=""
+# Chester
+elif [ $scl1 = "chester-login" ]; then
+  path="/lustre/scratch/sw/r/3.0.1.new/chest/gnu4.7.3/EXAMPLES/scripts.tar.gz"
+  tmpdir="/lustre/scratch/$USER/R_TEMP"
 fi
 
 
