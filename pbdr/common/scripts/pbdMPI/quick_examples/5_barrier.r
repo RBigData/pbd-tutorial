9b27984f40b8d8857fc065706a267fb946d45353
George Ostrouchov <georgeost@gmail.com>
2014-02-27 11:57:16 -0500

    after Japan

--- pbdr/common/scripts/pbdMPI/quick_examples/5_barrier.r
+++ pbdr/common/scripts/pbdMPI/quick_examples/5_barrier.r
@@ -4,15 +4,15 @@ init()
 .SPMD.CT$msg.barrier <- TRUE
 .SPMD.CT$print.quiet <- TRUE
 
-for (rank in 1:comm.size()-1){
+for (rank in 1:comm.size() - 1){
   if (comm.rank() == rank){
-    cat(paste("Hello", rank+1, "of", comm.size(), "\n"))
+    cat(paste("HelloA", rank+1, "of", comm.size(), "\n"))
   }
   barrier()
 }
 
-comm.cat("\n")
+comm.cat("newline\n")
 
-comm.cat(paste("Hello", comm.rank()+1, "of", comm.size(), "\n"), all.rank=TRUE)
+comm.cat(paste("HelloB", comm.rank()+1, "of", comm.size(), "\n"), all.rank=TRUE)
 
 finalize()
